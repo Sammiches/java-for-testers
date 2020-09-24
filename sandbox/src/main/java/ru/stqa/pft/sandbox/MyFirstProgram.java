2202c1671e76c492dc722e2465234f5498b01acf
13koshek <13koshek@yandex-team.ru>
2020-09-24 16:55:40 +0300

    Задание 2.3

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,15 +2,17 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 	public static void main(String[] args){
-		hello("user");
+		Point p1 = new Point(3,4);
+		Point p2 = new Point(1,2);
+		System.out.println("Расстояние между точкой с координатами " + p1.x + "," + p1.y + " и точкой с кординатами " + p2.x + "," + p2.y + " составляет " + distance(p1,p2));
 
-		Square s = new Square(5);
-		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
-		Rectangle r = new Rectangle(4,6);
-		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
-	}
-	public static void hello(String somebody){
-		System.out.println("Hello, "+ somebody + "!");
+//		Square s = new Square(5);
+//		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+//		Rectangle r = new Rectangle(4,6);
+//		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 	}
 
+	public static double distance(Point p1, Point p2){
+		return Math.sqrt((p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y));
+	}
 }
